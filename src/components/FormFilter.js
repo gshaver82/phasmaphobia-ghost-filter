ad07644504ebda103980798405afa6f3a585edca
gshaver <geneshaver@gmail.com>
2021-04-09 03:51:50 -0500

    added buttons, updates state correctly. no filter yet

---
+++ src/components/FormFilter.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import directory from "../directory.json";
+
+// function FormFilter(props) {
+//     return (
+//         <div>
+//             <h4>Filter by</h4>
+//             <form className="form">
+//                 <input
+//                     value={this.state.filterquery}
+//                     onChange={this.handleInputChange}
+//                     name="filterquery"
+//                     type="text"
+//                 />
+//                 <button onClick={this.handleFormSubmit}>Submit</button>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default FormFilter;
+
---
+++ src/components/Wrapper.js
@@ -0,0 +1,111 @@
+import React from "react";
+import directory from "../directory.json";
+
+class Wrapper extends React.Component {
+    // options for the ghost will be confirmed unknown or excluded clicking will cycle through the states
+    state = {
+        sortedList: directory,
+        Freezing: 'unknown',
+        EMF5: 'unknown',
+        Orbs: 'unknown',
+        SpiritBox: 'unknown',
+        GhostWriting: 'unknown',
+        Fingerprints: 'unknown',
+    };
+
+    // this function takes in the button value and assigns it to ghost attributes
+    //based on that value it changes the state from unknown to confirmed to excluded and back to unknown in that order
+    buttonPress = event => {
+        let ghostAttribute = event.target.value;
+        // console.log(ghostAttribute);
+        //gives and error identifier expected but that is a bogus error message
+        switch (this.state.[ghostAttribute]) {
+            case 'unknown':
+                console.log('unknown to confirmed');
+                this.setState({
+                    [ghostAttribute]: 'confirmed'
+                });
+                break;
+            case 'confirmed':
+                console.log('confirmed to excluded');
+                this.setState({
+                    [ghostAttribute]: 'excluded'
+                });
+                break;
+            case 'excluded':
+                console.log('excluded to unknown');
+                this.setState({
+                    [ghostAttribute]: 'unknown'
+                });
+                break;
+            default:
+                console.log('ERROR default reached');
+        }
+        this.renderList();
+    };
+
+    renderList = () => {
+
+        console.log('render list function reached');
+        console.log(this.state);
+        // this.setState({ sortedList: directory.sort((a, b) => (a.Freezing > b.Freezing) ? 1 : -1) })
+        // this.setState({ sortedList: directory.filter() })
+    };
+
+
+    render() {
+        return (
+            <div>
+                <div className="card-body">
+
+                    {/* button div */}
+                    <div>
+                        <button value="Freezing" onClick={this.buttonPress}>
+                            Freezing
+                        </button>{" "}
+                    </div>
+                    <div>
+                        <button value="EMF5" onClick={this.buttonPress}>
+                            EMF5
+                        </button>{" "}
+                    </div>
+                    <div>
+                        <button value="Orbs" onClick={this.buttonPress}>
+                            Orbs
+                        </button>{" "}
+                    </div>
+                    <div>
+                        <button value="SpiritBox" onClick={this.buttonPress}>
+                            Spirit Box
+                        </button>{" "}
+                    </div>
+                    <div>
+                        <button value="GhostWriting" onClick={this.buttonPress}>
+                            Ghost Writing
+                        </button>{" "}
+                    </div>
+                    <div>
+                        <button value="Fingerprints" onClick={this.buttonPress}>
+                            Fingerprints
+                        </button>{" "}
+                    </div>
+
+                    {/* button div */}
+
+
+
+                    {/* list div */}
+                    <ul className="list-group">
+                        {this.state.sortedList.map(result => (
+                            <li className="list-group-item" key={result.id}>
+                                {result.Name} --- {result.Strengths} --- {result.Weaknesses}
+                            </li>
+                        ))}
+                    </ul>
+                    {/* list div */}
+                </div>
+            </div>
+        );
+    }
+}
+export default Wrapper;
